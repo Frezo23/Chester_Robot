dfb880d8f30632843d4dcc988434435ce90391d6
*Frezo* <wilczewski.dominik@gmail.com>
2024-05-10 19:22:05 +0200

    Update Gui.py

--- Gui.py
+++ Gui.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import chess
 #from chessboard import display
 import random
+import time
 
 board = chess.Board()
 valid_fen = board.fen()
@@ -14,6 +15,7 @@ class ChessBoard(tk.Tk):
         self.title("Chess Board")
         self.configure(bg='#d5d5d5')
         self.geometry("1024x600")
+        self.attributes('-fullscreen', True)
         self.canvas = tk.Canvas(self, width=600, height=600, bg='#8f8f8f', highlightthickness=0)
         self.canvas.pack(anchor='w')
         self.selected_piece = None
@@ -59,6 +61,9 @@ class ChessBoard(tk.Tk):
 
         col = event.x // 75
         row = event.y // 75
+        
+        promotion = False
+        
         if self.selected_piece is None:
             self.selected_piece = (row, col)
             self.piece_to_move = self.canvas.find_closest(event.x, event.y)
@@ -70,14 +75,31 @@ class ChessBoard(tk.Tk):
             target_row, target_col = self.selected_piece
 
             move = self.algebraic_notation(target_row, target_col, row, col)
+            #print(move)
+            
+            decrypt_move = [m for m in move]
+            #print(decrypt_move)
+            #print(self.board[row][col])
+            #print(self.board[target_row][target_col])
+            if decrypt_move[3] == '8' and self.board[target_row][target_col] in ['P']:
+                move = move + 'q'
+                #print('new move:',move)
+                promotion = True
+            if decrypt_move[3] == '1' and self.board[target_row][target_col] in ['bp']:
+                move = move + 'q'
+                #print('new move:',move)
+                promotion = True
+                
+            
             moves = list(board.legal_moves) 
             moves = [move.uci() for move in moves]
-            print(moves)
+            #print(moves)
             if move in moves:
                 print('legal')
-                #valid_fen = board.fen()
                     #print(moves)
-                #board.push_san(str(move))
+                if promotion == True:
+                    board.push_san(str(move))
+                    valid_fen = board.fen()
                 #print(valid_fen)
                 #print(move)
 
@@ -94,31 +116,40 @@ class ChessBoard(tk.Tk):
                         
                     #print(self.board[target_row][target_col], self.board[row][col])
                     
-                    self.draw_pieces()
-                    
-                    print(self.selected_piece)
-                    print(self.piece_to_move)
+                    #print(row,col)
+                    #print(target_row,target_col)
+                    #print(self.selected_piece)
+                    #print(self.piece_to_move)
+                    #print(self.board[row][col])
+                    #print(self.board[target_row][target_col])
+                    if promotion == False:
+                        move = self.algebraic_notation(target_row, target_col, row, col)
+                        moves = list(board.legal_moves) 
+                        moves = [move.uci() for move in moves]
 
-                    move = self.algebraic_notation(target_row, target_col, row, col)
-                    moves = list(board.legal_moves) 
-                    moves = [move.uci() for move in moves]
-
-                        #print(moves)
-                    board.push_san(str(move))
-                    valid_fen = board.fen()
-                    print(valid_fen)
-                    print(move)
-                    print(self.board)
+                            #print(moves)
+                        board.push_san(str(move))
+                        valid_fen = board.fen()
+                    else:
+                        if self.board[row][col] == 'P':
+                            self.board[row][col] = 'Q'
+                        elif self.board[row][col] == 'bp':
+                            self.board[row][col] = 'bq'
+                        
+                    #print(valid_fen)
+                    #print(move)
+                    #print(self.board)
                     print(board)
+                    
+                    self.draw_pieces()
 
                     self.selected_piece = None
                     self.piece_to_move = None
-                    if (row == 0 or row == 7) and self.board[row][col] in ['P', 'bp']:  # Check if pawn reaches last row
-                        self.board[row][col] = 'Q' if self.board[row][col] == 'P' else 'bq'  # Promote pawn to queen
-
+                    promotion = False
             else:
                 self.selected_piece = None
                 self.piece_to_move = None
+                promotion = False
             
         #print(self.board)
     
